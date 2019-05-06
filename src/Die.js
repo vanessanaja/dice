3c52fcbe09b7b84f7e8962d7454005782b771d69
vanessanaja <vanessacnaja@gmail.com>
2019-05-06 16:14:31 -0700

    add animation to rolling dice

--- src/Die.js
+++ src/Die.js
@@ -2,12 +2,9 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component{
-  // constructor(props){
-  //   super(props)
-  // }
   render(){
     return (
-        <i className={`Die fas fa-dice-${this.props.face}`}></i>
+        <i className={`Die fas fa-dice-${this.props.face} ${this.props.rolling && 'shaking'}`}></i>
     );
   }
 }
